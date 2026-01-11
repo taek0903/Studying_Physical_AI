ebe3657d38f139ed7f6b4f22c21d5a8b9569a9e1
taek0903 <ico550001@gmail.com>
2026-01-11 15:12:53 +0900

    python_ch10_mex1_example

---
+++ ch10/mex1_example.py
@@ -0,0 +1,22 @@
+# mex1_example.py
+import mex1 # 모듈 불러오기
+
+# import sys
+# sys.path.append("D:/rokey") # 작업경로 지정
+
+# import python.ch09.Bag
+
+# 클래스 활용
+p2=mex1.Cvalue()
+p2.fprint()
+print(p2.lista)
+
+# 함수 활용
+value=mex1.plus(10,20)
+print(value)
+
+# 변수 활용
+# print(mex1.p1.lista)
+# mex1.p1.add(4)
+# mex1.p1.fprint()
+
