e5a3c9696e671aa9da5d7e796a5ab88c3d3d2df4
taek0903 <ico550001@gmail.com>
2026-01-11 14:40:33 +0900

    python_ch05_range1

---
+++ ch05/range1.py
@@ -0,0 +1,28 @@
+# range1.py
+print(range(10))
+print(list(range(10)))
+
+print(range(0,10))
+print(list(range(0,10)))
+
+print(range(3,10))
+print(list(range(3,10)))
+
+print(range(8,14,2))
+print(list(range(8,14,2)))
+
+print(list(range(1,10,3)))
+print(list(range(1,10,-1)))
+print(list(range(10,1,-1)))
+
+print("----------------------------")
+for i in range(1,3) :
+    print(i)
+
+print("----------------------------")
+for i in range(4):
+    print(i)
+
+print("----------------------------")
+for i in range(2,9,2):
+    print(i)
