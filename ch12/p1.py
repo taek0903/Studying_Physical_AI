6896c4433276ed23de9ea7d22f9f6e8689629229
taek0903 <ico550001@gmail.com>
2026-01-11 15:21:42 +0900

    python_ch12_p1

---
+++ ch12/p1.py
@@ -0,0 +1,7 @@
+# p1.py
+
+path=r"python\ch12\file3.txt"
+with open(path, "w", encoding="utf-8") as f:
+    f.write("김삿갓 597-89-00089\n")
+    f.write("이수근 343-64-00064\n")
+    f.write("박혁거세 136-97-00097\n")
