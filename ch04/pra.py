7f945ec90f2714ab01b830bec4bfce4dcb7b33e8
taek0903 <ico550001@gmail.com>
2026-01-11 14:37:55 +0900

    python_ch04_practice

---
+++ ch04/pra.py
@@ -0,0 +1,52 @@
+# 1번
+nums = [1, 2, 3]
+print(nums)
+
+# 2번
+fruits = []
+print(fruits)
+
+fruits.append('자몽')
+print(fruits)
+
+fruits.append('멜론')
+print(fruits)
+
+fruits.append('레몬')
+print(fruits)
+
+# 3번
+fruits = ['자몽', '멜론', '레몬']
+print(fruits[1])
+
+# 4번
+fruits = ['자몽', '멜론', '레몬']
+print(fruits)
+del fruits[0]
+
+# 5번
+nums = (1,2,3)
+
+# 6번
+my_tuple = (3.14, 2.71)
+print(my_tuple)
+print(my_tuple[1])
+
+# 7번
+## 7-1
+my_var = 1,
+print(type(my_var))
+## 7-2
+my_var =(1,)
+print(type(my_var))
+## 7-3
+my_var=(1)
+print(type(my_var))
+## 7-4
+my_var = 1,2
+print(type(my_var))
+
+# 8번
+alice = {'성별':'여', '나이':'13', '혈액형':'AB'}
+alice['나이']=14
+print(alice['나이'])
