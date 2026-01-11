f63e4d047b59115e928f2a5cdb7056676b5228d0
taek0903 <ico550001@gmail.com>
2026-01-11 14:30:40 +0900

    python_ch02_operator

---
+++ ch02/operator.py
@@ -0,0 +1,26 @@
+#oporator.py
+
+print(1+2)
+print(3-2)
+print(2*4)
+print(6/3)
+# 나누기 연산 결과 => 실수(float)
+
+print(5**2)
+print(5//2)
+print(5%2)
+
+print(3+1/2)
+print(3-1+2)
+print(3*1%2)
+
+num1 = 9
+num2 = 2
+print(num1//num2)
+print(num1 % num2)
+
+print(8/2)
+print(2*2)
+print(8.0/2)
+print(8.5//2)
+
