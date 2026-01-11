c47c326f5223a856dad9a78875e1adf63c3cf64e
taek0903 <ico550001@gmail.com>
2026-01-11 14:36:12 +0900

    python_ch03_week

---
+++ ch03/week.py
@@ -0,0 +1,20 @@
+# week.py
+
+print("월요일 부터 일요일 중 영어로 번역하고 싶은 요일을 입력하세요.")
+yoil = input("요일을 입력하세요.")
+if yoil == "월요일" :
+    print("Monday")
+elif yoil == "화요일" :
+    print("Tuseday")
+elif yoil == "수요일" :
+    print("Wednesday")
+elif yoil == "목요일" :
+    print("Thrusday")
+elif yoil == "금요일" :
+    print("Friday")
+elif yoil == "토요일" :
+    print("Saturday")
+elif yoil == "일요일" :
+    print("Sunday")
+else :
+    print("한글 요일을 잘못 입력하셨습니다.")
