3abe89ecae21758159e7c9cf435312b02b7272eb
taek0903 <ico550001@gmail.com>
2026-01-11 15:30:26 +0900

    python_ch16_queue_list

---
+++ ch16/queue_list.py
@@ -0,0 +1,19 @@
+# queue_list.py
+queue = []
+
+# Enqueue
+queue.append(1)
+queue.append(2)
+queue.append(3)
+queue.append(4)
+print(f'queue= {queue}')
+
+# Dequeue
+queue.pop(0)
+print(f'queue= {queue}')
+queue.pop(0)
+print(f'queue= {queue}')
+queue.pop(0)
+print(f'queue= {queue}')
+queue.pop(0)
+print(f'queue= {queue}')
