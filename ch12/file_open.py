51b0455be9b0ea781aaca3b7cf1f9e0647997a23
taek0903 <ico550001@gmail.com>
2026-01-11 15:20:13 +0900

    python_ch12_file_open

---
+++ ch12/file_open.py
@@ -0,0 +1,9 @@
+# file_open.py
+
+# f=open("D:/rokey/python/ch12/file1.txt","w")
+# f=open(r"D:\rokey\python\ch12\file1.txt","w")
+
+# 현재 작업 디렉터리 => D:\rokey
+# f = open(r"python\ch12\file1.txt","w")
+f = open(r".\python\ch12\file1.txt","w")
+f.close()
