5443d6c2810fbc10b01f2beeeae78e8fec4d6003
taek0903 <ico550001@gmail.com>
2026-01-11 15:08:20 +0900

    python_ch08_selectSorting2

---
+++ ch08/selectSorting2.py
@@ -0,0 +1,32 @@
+# selectSorting2.py
+
+ca=[21,10,11,15,13]
+mina=ca[0]
+minix=0
+for sb in range(1,5,1):
+    if mina > ca[sb]:
+        mina = ca[sb]
+        minix = sb
+print(ca)
+
+temp= ca[0]
+ca[0]= ca[minix]
+ca[minix]=temp
+print(ca)
+# 1차 목표 : [10,21,11,15,13]
+
+print('-------------------')
+mina= ca[1]
+minix = 1
+
+for sb in range(2,5,1):
+    if mina > ca[sb]:
+        mina = ca[sb]
+        minix = sb
+print(ca)
+
+temp = ca[1]
+ca[1]=ca[minix]
+ca[minix]=temp
+print(ca)
+# 2차 목표 : [10, 11, 21, 15, 13]
