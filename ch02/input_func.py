b8347e50e79d0fe6ea2549dd6dbb611d1c497064
taek0903 <ico550001@gmail.com>
2026-01-11 14:28:01 +0900

    python_ch02_input_function

---
+++ ch02/input_func.py
@@ -0,0 +1,11 @@
+# input_func.py
+
+# 반환값 없음
+sb = print("hi") # 변수 저장 불가능
+print(sb)        # None
+
+# 반환값 있음
+# 변수 있음
+sa = input("문자열을 입력하시오: ")  # input("가이드 멘트")
+print("입력된 값은 = ", end=" ")
+print(sa)
