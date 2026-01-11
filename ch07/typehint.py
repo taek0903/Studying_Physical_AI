b39d871614dcba0096a1bfbeabf23b3563caadc9
taek0903 <ico550001@gmail.com>
2026-01-11 15:05:54 +0900

    python_ch07_typehint

---
+++ ch07/typehint.py
@@ -0,0 +1,12 @@
+# typehint.py
+
+def add(x,y):
+    return x+y
+
+# def add(x:int, y:int)->int:
+#     return x + y
+
+print(add(3,5))
+print(type(add(3,5)))
+print(add(3.2,5))
+print(type(add(3.2,5)))
