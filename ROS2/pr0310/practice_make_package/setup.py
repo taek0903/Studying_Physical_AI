74ce1ff9b3e2369745aa08cd815cc54d3b633acc
taek0903 <ico550001@gmail.com>
2026-03-10 22:52:06 +0900

    ROS2_pr0310

---
+++ ROS2/pr0310/practice_make_package/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'practice_make_package'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='taek',
+    maintainer_email='taek@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'practice_make_node = practice_make_package.practice_make_node:main'
+        ],
+    },
+)
